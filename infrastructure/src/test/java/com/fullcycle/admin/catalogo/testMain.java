b6081a3d39d720bef5c5ad5ba2b06f62a32076c1
Gustavo Giongo <ggiongo4@gmail.com>
2024-03-27 06:29:22 -0300

    Configurações Flyway

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/testMain.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/testMain.java
@@ -2,11 +2,13 @@ package com.fullcycle.admin.catalogo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 public class testMain {
     @Test
     public void testMain(){
         Assertions.assertNotNull(new Main());
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Main.main(new String[]{});
     }
 }
